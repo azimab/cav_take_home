4add85d985ef0d15e9b7559e1484c003f28cd94a
azimab <abdulmajeethazim9@gmail.com>
2025-04-20 13:15:48 -0400

    Added calculations for wheel slip ratios, imu jitter and lap time

--- src/take_home_node/include/take_home_node/take_home.hpp
+++ src/take_home_node/include/take_home_node/take_home.hpp
@@ -8,16 +8,47 @@
 #include <rclcpp/node_options.hpp>
 #include <rclcpp/rclcpp.hpp>
 
+#include <raptor_dbw_msgs/msg/wheel_speed_report.hpp>
+#include <raptor_dbw_msgs/msg/steering_extended_report.hpp>
+
+#include <novatel_oem7_msgs/msg/rawimu.hpp>
+
 class TakeHome : public rclcpp::Node {
  public:
   TakeHome(const rclcpp::NodeOptions& options);
 
   void odometry_callback(nav_msgs::msg::Odometry::ConstSharedPtr odom_msg);
+  
+  void wheel_speed_callback(raptor_dbw_msgs::msg::WheelSpeedReport::ConstSharedPtr wspeed_msg);
+
+  void steering_callback(raptor_dbw_msgs::msg::SteeringExtendedReport::ConstSharedPtr steering_msg);
+
+  void imu_callback(novatel_oem7_msgs::msg::RAWIMU::ConstSharedPtr imu_msg);
+
+  void lap_time_callback(std_msgs::msg::Float32::ConstSharedPtr dist_msg);
 
  private:
 
   // Subscribers and Publishers
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscriber_;
-  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr metric_publisher_;
+  rclcpp::Subscription<raptor_dbw_msgs::msg::WheelSpeedReport>::SharedPtr wheel_speed_subscriber_;
+  rclcpp::Subscription<raptor_dbw_msgs::msg::SteeringExtendedReport>::SharedPtr steering_angle_subscriber_;
+  rclcpp::Subscription<novatel_oem7_msgs::msg::RAWIMU>::SharedPtr imu_subscriber_;
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr lap_time_subscriber_;
 
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr metric_publisher_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr rr_slip_publisher_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr rl_slip_publisher_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr fl_slip_publisher_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr fr_slip_publisher_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr imu_publisher_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr lap_time_publisher_;
+  //Declare constant values and variables 
+  const float wf = 1.638;
+  const float wr = 1.523;
+  const float lf = 1.7238;
+  float vw_rr, vw_rl, vw_fr,vw_fl, steering_angle;
+  std::vector<double> times;
+  double lap_start = 0.0;
+  bool first_lap = true;
 };
--- src/take_home_node/src/take_home.cpp
+++ src/take_home_node/src/take_home.cpp
@@ -1,6 +1,7 @@
 #include "take_home_node/take_home.hpp"
 #include <rclcpp_components/register_node_macro.hpp>
-
+#include <cmath>
+#include <vector>
 TakeHome::TakeHome(const rclcpp::NodeOptions& options)
     : Node("take_home_metrics", options) {
 
@@ -13,7 +14,98 @@ TakeHome::TakeHome(const rclcpp::NodeOptions& options)
       "vehicle/uva_odometry", qos_profile,
       std::bind(&TakeHome::odometry_callback, this, std::placeholders::_1));
 
-      metric_publisher_ = this->create_publisher<std_msgs::msg::Float32>("metrics_output", qos_profile);
+    wheel_speed_subscriber_ = this->create_subscription<raptor_dbw_msgs::msg::WheelSpeedReport>(
+      "/raptor_dbw_interface/wheel_speed_report", qos_profile,
+      std::bind(&TakeHome::wheel_speed_callback, this, std::placeholders::_1));
+      
+    steering_angle_subscriber_ = this->create_subscription<raptor_dbw_msgs::msg::SteeringExtendedReport>(
+      "/raptor_dbw_interface/steering_extended_report", qos_profile,
+      std::bind(&TakeHome::steering_callback, this, std::placeholders::_1));
+    
+    imu_subscriber_ = this->create_subscription<novatel_oem7_msgs::msg::RAWIMU>(
+      "novatel_top/rawimu", qos_profile,
+      std::bind(&TakeHome::imu_callback, this, std::placeholders::_1));
+
+    lap_time_subscriber_ = this->create_subscription<std_msgs::msg::Float32>(
+      "curvilinear_distance", qos_profile,
+      std::bind(&TakeHome::lap_time_callback, this, std::placeholders::_1));
+    
+
+    metric_publisher_ = this->create_publisher<std_msgs::msg::Float32>("metrics_output", qos_profile);
+    rr_slip_publisher_ = this->create_publisher<std_msgs::msg::Float32>("slip/long/rr", qos_profile);
+    rl_slip_publisher_ = this->create_publisher<std_msgs::msg::Float32>("slip/long/rl", qos_profile);
+    fl_slip_publisher_ = this->create_publisher<std_msgs::msg::Float32>("slip/long/fl", qos_profile);
+    fr_slip_publisher_ = this->create_publisher<std_msgs::msg::Float32>("slip/long/fr", qos_profile);
+    imu_publisher_ = this->create_publisher<std_msgs::msg::Float32>("imu_top/jitter",qos_profile);
+    lap_time_publisher_ = this->create_publisher<std_msgs::msg::Float32>("lap_time", qos_profile);
+
+}
+
+
+void TakeHome::wheel_speed_callback(raptor_dbw_msgs::msg::WheelSpeedReport::ConstSharedPtr wspeed_msg) {
+  //save and convert the wheel speeds to m/s
+    vw_rr = wspeed_msg->rear_right * 1000.0 / 3600.0;
+    vw_rl = wspeed_msg->rear_left * 1000.0 / 3600.0;
+    vw_fr = wspeed_msg->front_right * 1000.0 / 3600.0;
+    vw_fl = wspeed_msg->front_left * 1000.0 / 3600.0;
+}
+
+void TakeHome::steering_callback(raptor_dbw_msgs::msg::SteeringExtendedReport::ConstSharedPtr steering_msg) {
+  //get the steering wheel angle and convert to radians
+    steering_angle = (steering_msg->primary_steering_angle_fbk / 15.0) * M_PI  / 180.0;
+}
+
+
+void TakeHome::imu_callback(novatel_oem7_msgs::msg::RAWIMU::ConstSharedPtr imu_msg) {
+    rclcpp::Time stamp  = imu_msg->header.stamp;
+    double current = stamp.seconds();
+    //fully clearing the timestamps if the bag is reset for any reason. 
+    if (!times.empty() && (current - times.back()) < -1.0) {
+        times.clear();
+    }
+    //erase times outside of the 1 second window.
+    times.push_back(current);
+    while ((current - times.front()) > 1.0 && !times.empty()) {
+      times.erase(times.begin());
+    }
+    //calculate and publish the jitter using the variance equation if there are at least two timestamps to use in the array.
+    if (times.size() >= 2) {
+      double sum = 0;
+      double mean = 0;
+      double variance = 0;
+      for (size_t i = 1; i < times.size(); i++) {
+        sum += times[i] - times[i-1];
+      }
+      mean = sum/(times.size() - 1);
+
+      for (size_t i = 1; i < times.size(); i++) {
+        double x = times[i] - times[i-1];
+        variance += (x - mean) * (x - mean);
+      }
+      variance = variance / (times.size() -1);
+
+      std_msgs::msg::Float32 jitter_msg;
+      jitter_msg.data = variance;
+      imu_publisher_->publish(jitter_msg);
+    }
+}
+
+void TakeHome::lap_time_callback(std_msgs::msg::Float32::ConstSharedPtr dist_msg) {
+  float distance = dist_msg->data;
+  rclcpp::Time timestamp = this->now();
+  double current_time = timestamp.seconds();
+  if (first_lap) {
+    lap_start = current_time;
+    first_lap = false;
+  }
+  //Check to see if curvilinear distance was reset. 
+  if (distance == 0.0 && (current_time-lap_start) >= 1) {
+    double lap_time = (current_time - lap_start);
+    std_msgs::msg::Float32 lap_msg;
+    lap_msg.data = lap_time;
+    lap_time_publisher_->publish(lap_msg);
+    lap_start = current_time;
+  }
 }
 
 // 
@@ -28,12 +120,48 @@ void TakeHome::odometry_callback(nav_msgs::msg::Odometry::ConstSharedPtr odom_ms
   float position_x = odom_msg->pose.pose.position.x;
   float position_y = odom_msg->pose.pose.position.y;
   float position_z = odom_msg->pose.pose.position.z;
+//calculate each wheel slip ratio
+  float vx = odom_msg->twist.twist.linear.x;
+  float vy = odom_msg->twist.twist.linear.y;
+  float w = odom_msg->twist.twist.angular.z;
+
+  float v_rr = vx - 0.5 * w * wr;
+  float k_rr = (vw_rr-v_rr) / v_rr;
+
+  float v_rl = vx + 0.5 * w * wr;
+  float k_rl = (vw_rl-v_rl) / v_rl;
+
+  float vx_fr  = vx - 0.5 * w * wf;
+  float vy_fr  = vy + w * lf;
+  float vdx_fr = cos(steering_angle)*vx_fr - sin(steering_angle)*vy_fr;
+  float k_fr = (vw_fr - vdx_fr)/vdx_fr;
+
+  float vx_fl  = vx + 0.5 * w * wf;
+  float vy_fl  = vy + w * lf;
+  float vdx_fl = cos(steering_angle)*vx_fl - sin(steering_angle)*vy_fl;
+  float k_fl = (vw_fl - vdx_fl)/vdx_fl;
 
   // Do stuff with this callback! or more, idc
   std_msgs::msg::Float32 metric_msg;
   metric_msg.data = (position_x + position_y + position_z) / (position_x + position_z); // Example metric calculation
   metric_publisher_->publish(metric_msg);
+  //Publish to topics for each wheel slip ratio
+  std_msgs::msg::Float32 rr_msg;
+  rr_msg.data = k_rr;
+  rr_slip_publisher_->publish(rr_msg);
+
+  std_msgs::msg::Float32 rl_msg;
+  rl_msg.data = k_rl;
+  rl_slip_publisher_->publish(rl_msg);
+
+  std_msgs::msg::Float32 fr_msg;
+  fr_msg.data = k_fr;
+  fr_slip_publisher_->publish(fr_msg);
+
+  std_msgs::msg::Float32 fl_msg;
+  fl_msg.data = k_fl;
+  fl_slip_publisher_->publish(fl_msg);
+
+  
 }
-
-
 RCLCPP_COMPONENTS_REGISTER_NODE(TakeHome)
